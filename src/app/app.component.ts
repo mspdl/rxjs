f30936659d4e5f3c8dc6ee99af158a9dcf637036
Morgan <morgan.espindola@gmail.com>
2022-07-27 22:25:06 +0100

    ReplaySubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +8,10 @@ import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'rxjs';
-  mySubject$!: BehaviorSubject<any>;
+  mySubject$!: ReplaySubject<any>;
 
   ngOnInit() {
-    this.mySubject$ = new BehaviorSubject(200);
+    this.mySubject$ = new ReplaySubject();
     this.mySubject$.subscribe((x) => console.log('First subscribe', x));
     this.mySubject$.next(1);
     this.mySubject$.next(2);
